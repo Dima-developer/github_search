b80f504ccba653c36436044581e8ff88d527a065
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-10-21 19:26:47 +0300

    Redux p.2

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import {Navbar} from "./components/Navbar";
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {Profile} from "./pages/Profile";
+import {Alert} from "./components/Alert";
+import {AlertState} from "./context/alert/alertState";
+import {GithubState} from "./context/github/githubState";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Navbar />
-          <div className="container pt-4">
-          <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/profile/:name" component={Profile} />
-          </Switch>
+    return (
+        <GithubState>
+            <AlertState>
+                <BrowserRouter>
+                    <Navbar/>
+                    <div className="container pt-4">
+                        <Alert alert={{text: 'Test alert'}}/>
+                        <Switch>
+                            <Route path="/" exact component={Home}/>
+                            <Route path="/about" component={About}/>
+                            <Route path="/profile/:name" component={Profile}/>
+                        </Switch>
 
-        </div>
-      </BrowserRouter>
-  );
+                    </div>
+                </BrowserRouter>
+            </AlertState>
+        </GithubState>
+    );
 }
 
 export default App;
